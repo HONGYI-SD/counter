3f8c8de14d7d135269607a62373b6b137b26c84b
Winter <huangdongdong910@163.com>
2024-08-08 23:38:35 +0800

    ops: add test codes

--- anchor-L2/tests/counter_anchor.ts
+++ anchor-L2/tests/counter_anchor.ts
@@ -12,90 +12,46 @@ describe('counter_anchor', () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
   const payer = provider.wallet as anchor.Wallet;
-
   const program = anchor.workspace.CounterAnchor as Program<CounterAnchor>;
   console.log("program id:", program.programId.toString())
 
-  //const signer = anchor.web3.Keypair.generate();
-  const connection = anchor.getProvider().connection;
- 
-  // const treeKeypair = new Keypair();
-  // console.log("merkle tree account pubkey:", treeKeypair.publicKey.toString())
-  // const secretKeyString = JSON.stringify(Array.from(treeKeypair.secretKey));
-  // console.log("merkle tree account secretKeyString:", secretKeyString)
-
   const secretKeyString = 
-  "[248,206,172,123,40,211,229,101,31,246,109,160,157,29,195,83,230,153,120,114,69,20,8,39,19,163,188,148,121,93,170,64,190,32,135,116,84,18,23,80,146,53,106,65,97,205,216,6,37,183,146,236,100,77,180,35,249,45,211,249,218,53,33,139]"
+  "[98,238,89,234,58,12,104,177,42,244,30,247,110,184,42,74,187,207,8,137,243,202,179,126,4,180,208,42,40,192,66,26,250,234,96,229,144,203,134,254,50,176,231,30,91,97,200,251,143,79,71,27,238,176,119,60,7,7,130,92,108,254,91,254]"
   const summaryKeypair = Keypair.fromSecretKey(new Uint8Array(JSON.parse(secretKeyString)))
   console.log("merkle tree account pubkey:", summaryKeypair.publicKey.toString())
-
   const localTree = new MerkleTree(HashingAlgorithm.Sha256d, 32);
 
   it('Increment Counter', async () => {
     try {
-      const listenerEvent2 = program.addEventListener("depositEvent", async (event, _slot, _sig) => {
-        const depositIndex = event.depositIndex.toNumber() - 1;
-        console.log("event depositIndex: ", depositIndex);
-        console.log("event leafAccountPubkey: ", event.leafAccountPubkey.toString());
-        console.log("event merkle root: ", event.merkleRoot.toString());
-        console.log('event amount:', event.amount.toNumber());
-        console.log("event user", event.user.toString());
-        
-        // const addrU8Arr = bs58.decode(event.addr.toString());
-        // console.log("addrU8Arr: ", addrU8Arr.toString());
-        // const amountByteArr = event.amount.toArray('le', 8);
-        // const amountUint8Array = new Uint8Array(amountByteArr);
-        // const totalU8Arr = new Uint8Array(amountUint8Array.length + addrU8Arr.length);
-        // totalU8Arr.set(amountUint8Array);
-        // totalU8Arr.set(addrU8Arr, amountUint8Array.length);
-        // localTree.add_leaf(totalU8Arr);
-        // localTree.merklize();
-        // console.log("new root:", localTree.get_merkle_root().toString());
-
-
-        // if (eventIdx > 0) {
-        //   const proof: MerkleProof = localTree.merkle_proof_index(eventIdx);
-        //   console.log("pairing hashes: ", proof.get_pairing_hashes());
-        //   console.log("pairing hashes length: ", proof.get_pairing_hashes().length);
-
-        //   // send to verify
-        //   let proof_hashes = proof.get_pairing_hashes();
-        //   await program.methods.verifyMerkleProof(new BN(event.amount), event.addr, eventIdx, Buffer.from(proof_hashes)).accounts({ user: payer.publicKey, merkleTree: treeKeypair.publicKey }).rpc();
-        //   console.log("over !!!");
-
-        // }
-      });
-      
-      const addrU8Arr = bs58.decode(payer.publicKey.toString());
-      console.log("addrU8Arr: ", addrU8Arr.toString());
-      const depositAmount = new BN(1);
-      const amountByteArr = depositAmount.toArray('le', 8);
-      const amountUint8Array = new Uint8Array(amountByteArr);
-      const totalU8Arr = new Uint8Array(amountUint8Array.length + addrU8Arr.length);
-      totalU8Arr.set(amountUint8Array);
-      totalU8Arr.set(addrU8Arr, amountUint8Array.length);
-      localTree.add_leaf(totalU8Arr);
+      const depositIndex = 8;
+      for (let i = 0; i < depositIndex; i++) {
+        const depositUserU8Arr = bs58.decode(payer.publicKey.toString());
+        const amountByteArr = new BN(i).toArray('le', 8);
+        const amountUint8Array = new Uint8Array(amountByteArr);
+        const depositItem = new Uint8Array(amountUint8Array.length + depositUserU8Arr.length);
+        depositItem.set(amountUint8Array);
+        depositItem.set(depositUserU8Arr, amountUint8Array.length);
+        localTree.add_leaf(depositItem);
+      }
       localTree.merklize();
       let root = localTree.get_merkle_root();
-
-      const depositIndex = new BN(1);
-      await program.methods.updateLeafpdaMerkleRoot(Buffer.from(root), depositIndex)
+      console.log("client root:", root);
+      await program.methods.updateLeafpdaMerkleRoot(Buffer.from(root), new BN(depositIndex-1))
       .accounts({l2Summary: summaryKeypair.publicKey})
       .rpc();
 
-      const proof: MerkleProof = localTree.merkle_proof_index(0);
+      const randomIndex = Math.floor(Math.random() * 8);
+      console.log("random index: ", randomIndex);
+
+      const proof: MerkleProof = localTree.merkle_proof_index(randomIndex);
       let proof_hashes = proof.get_pairing_hashes();
-
-      await program.methods.verifyMerkleProof(depositAmount, depositIndex.toNumber(), payer.publicKey, Buffer.from(proof_hashes))
+      const depositAmount = randomIndex;
+      await program.methods.verifyMerkleProof(new BN(depositAmount), randomIndex, payer.publicKey, Buffer.from(proof_hashes))
       .accounts({l2Summary: summaryKeypair.publicKey})
       .rpc();
 
-      await new Promise((resolve) => setTimeout(resolve, 1000*1));
-      program.removeEventListener(listenerEvent2);
     } catch (error) {
       console.log('error:', error.toString())
     }
-
   });
-
 });
