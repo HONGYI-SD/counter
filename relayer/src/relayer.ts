66097f08e5caf7795449bb336d9b2418504d593d
Winter <huangdongdong910@163.com>
2024-08-15 15:52:51 +0800

    ops: update code

--- relayer/src/relayer.ts
+++ relayer/src/relayer.ts
@@ -3,13 +3,19 @@
 import * as anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
 import { Keypair } from '@solana/web3.js';
+import bs58 from 'bs58';
 import idlL1 from '../../anchor/target/idl/counter_anchor.json';
 import idlL2 from '../../anchor-L2/target/idl/counter_anchor.json';
 import type { CounterAnchor } from '../../anchor/target/types/counter_anchor';
 import type { CounterAnchor as CounterAnchorL2} from '../../anchor-L2/target/types/counter_anchor';
 import { program } from '@coral-xyz/anchor/dist/cjs/native/system';
+import { HashingAlgorithm, MerkleTree, MerkleProof } from '../../../svm-merkle-tree/dist/node/svm_merkle_tree'
 const fs = require("fs");
 const path = require("path");
+const CHUNK_SIZE = 10;
+const L2SUMMARYPUBKEY = "";
+const MINTPUBKEY = "";
+const USERTOKENACCOUNTPUBKEY = "";
 
 const l1ClusterUrl = "http://127.0.0.1:8899";
 const l1Connection = new anchor.web3.Connection(l1ClusterUrl, "confirmed");
@@ -37,6 +43,8 @@ console.log("l2Payer: ", l2Payer.publicKey.toString());
 const programL2 = new Program(idlL2 as anchor.Idl, l2Provider) as unknown as Program<CounterAnchorL2>;
 console.log("l2 program id: ", programL2.programId.toString());
 
+const localTree = new MerkleTree(HashingAlgorithm.Sha256d, 32);
+
 const initRelayer = async () => {
     // todo: init db
 }
@@ -58,30 +66,45 @@ const listenEvent = async () => {
             console.log('event amount:', event.amount.toNumber());
             console.log("event user", event.user.toString());
             
-            // const addrU8Arr = bs58.decode(event.addr.toString());
-            // console.log("addrU8Arr: ", addrU8Arr.toString());
-            // const amountByteArr = event.amount.toArray('le', 8);
-            // const amountUint8Array = new Uint8Array(amountByteArr);
-            // const totalU8Arr = new Uint8Array(amountUint8Array.length + addrU8Arr.length);
-            // totalU8Arr.set(amountUint8Array);
-            // totalU8Arr.set(addrU8Arr, amountUint8Array.length);
-            // localTree.add_leaf(totalU8Arr);
-            // localTree.merklize();
-            // console.log("new root:", localTree.get_merkle_root().toString());
-    
-    
-            // if (eventIdx > 0) {
-            //   const proof: MerkleProof = localTree.merkle_proof_index(eventIdx);
-            //   console.log("pairing hashes: ", proof.get_pairing_hashes());
-            //   console.log("pairing hashes length: ", proof.get_pairing_hashes().length);
-    
-            //   // send to verify
-            //   let proof_hashes = proof.get_pairing_hashes();
-            //   await program.methods.verifyMerkleProof(new BN(event.amount), event.addr, eventIdx, Buffer.from(proof_hashes)).accounts({ user: payer.publicKey, merkleTree: treeKeypair.publicKey }).rpc();
-            //   console.log("over !!!");
-    
-            // }
+            if (depositIndex % CHUNK_SIZE === 0){
+                localTree.free();
+                console.log("reset merkle tree, reached chunk size ", depositIndex);
+            }
+            const addrU8Arr = bs58.decode(event.user.toString());
+            console.log("addrU8Arr: ", addrU8Arr.toString());
+            const amountByteArr = event.amount.toArray('le', 8);
+            const amountUint8Array = new Uint8Array(amountByteArr);
+            const totalU8Arr = new Uint8Array(amountUint8Array.length + addrU8Arr.length);
+            totalU8Arr.set(amountUint8Array);
+            totalU8Arr.set(addrU8Arr, amountUint8Array.length);
+            localTree.add_leaf(totalU8Arr);
+            localTree.merklize();
             
+            console.log("new root:", localTree.get_merkle_root().toString());
+    
+    
+            const proof: MerkleProof = localTree.merkle_proof_index(depositIndex);
+            console.log("pairing hashes: ", proof.get_pairing_hashes());
+            console.log("pairing hashes length: ", proof.get_pairing_hashes().length);
+
+            // send to verify
+            let proof_hashes = proof.get_pairing_hashes();
+            await programL2.methods.verifyMerkleProof(
+                new anchor.BN(event.amount), 
+                depositIndex, 
+                event.user, 
+                Buffer.from(proof_hashes)
+            )
+            .accounts({
+                l2Summary: new anchor.web3.PublicKey(L2SUMMARYPUBKEY),
+                mint: new anchor.web3.PublicKey(MINTPUBKEY),
+                userTokenAccount: new anchor.web3.PublicKey(USERTOKENACCOUNTPUBKEY),
+                // @ts-ignore
+                tokenProgram: TOKEN_PROGRAM_ID, 
+            })
+            .signers([l2Payer.payer])
+            .rpc();
+    
           });
         
         await new Promise((resolve) => setTimeout(resolve, 1000*60*5));
